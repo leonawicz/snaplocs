a2b3b7a6ad89a2a3fbf2c04d78133e8505d90b8f
leonawicz <matt.rstat@gmail.com>
2017-09-29 09:28:53 -0600

    Add package doc.

---
+++ R/snaplocs.R
@@ -0,0 +1,13 @@
+#' snaplocs: Functions for working with SNAP point location data.
+#'
+#' The \code{snaplocs} package provides functions for working with point location data used in various applications and projects by the
+#' Scenarios Network for Alaska and Arctic Planning at the University of Alaska Fairbanks. Point location data is included.
+#' Locations include major and minor cities, towns, villages, and some other locations such as mines.
+#' This package is part of the SNAPverse collection of R packages.
+#'
+#' @docType package
+#' @name snaplocs
+NULL
+
+#' @importFrom magrittr %>%
+NULL
