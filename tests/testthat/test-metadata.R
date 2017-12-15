500d687a35ecc96f2902f647fdab76b368636f09
leonawicz <matt.rstat@gmail.com>
2017-12-15 12:10:50 -0700

    Update unit tests.

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -2,56 +2,56 @@ context("metadata")
 
 test_that("metadata helpers return as expected", {
   x <- c("Fairbanks", "Calgary", "Vancouver", "Whitehorse")
-  reg <- c("Alaska", "Alberta", "British Columbia", "Yukon")
+  grp <- c("Alaska", "Alberta", "British Columbia", "Yukon")
   country <- c("United States", rep("Canada", 3))
-  purrr::walk2(x, reg, ~expect_identical(get_region(.x), .y))
+  purrr::walk2(x, grp, ~expect_identical(get_state(.x), .y))
   purrr::walk2(x, country, ~expect_identical(get_country(.x), .y))
   purrr::walk(x, ~expect_is(get_coords(.x), "tbl_df"))
   purrr::walk(x, ~expect_identical(dim(get_coords(.x)), c(1L, 2L)))
 
   x <- c("Fairbanks", "Calgary", "Vancouver", "Whitehorse")
-  reg <- c("Alaska", "Alberta", "British Columbia", "Yukon")
+  grp <- c("Alaska", "Alberta", "British Columbia", "Yukon")
   country <- c("United States", rep("Canada", 3))
-  expect_identical(get_region(x), reg)
+  expect_identical(get_province(x), grp)
   expect_identical(get_country(x), country)
   expect_is(get_coords(x), "tbl_df")
   expect_identical(dim(get_coords(x)), c(length(x), 2L))
 
   x <- "A"
   err <- paste0("'", x, "' is not an available location in `locs`.")
-  expect_error(get_region(x), err)
+  expect_error(get_state(x), err)
   expect_error(get_country(x), err)
   expect_error(get_coords(x), err)
 
   x <- c("Fairbanks", "A")
   err <- paste0("At least one location is not an available location in `locs`.")
-  expect_error(get_region(x), err)
+  expect_error(get_state(x), err)
   expect_error(get_country(x), err)
   expect_error(get_coords(x), err)
 })
 
-test_that("metadata region argument works", {
+test_that("metadata group argument works", {
   x <- rep(c("Fairbanks", "Galena"), 2)
-  reg <- rep(c("Alaska", "British Columbia"), each = 2)
+  grp <- rep(c("Alaska", "British Columbia"), each = 2)
   country <- rep(c("United States", "Canada"), each = 2)
   good <- c(1:2, 4)
   bad <- 3
-  purrr::walk2(x[good], reg[good], ~expect_identical(get_region(.x, .y), .y))
-  purrr::walk(good, ~expect_identical(get_country(x[.x], reg[.x]), country[.x]))
+  purrr::walk2(x[good], grp[good], ~expect_identical(get_state(.x, .y), .y))
+  purrr::walk(good, ~expect_identical(get_country(x[.x], grp[.x]), country[.x]))
   purrr::walk(x[good], ~expect_is(get_coords(.x, .y), "tbl_df"))
-  purrr::walk2(x[good], reg[good], ~expect_identical(dim(get_coords(.x, .y)), c(1L, 2L)))
+  purrr::walk2(x[good], grp[good], ~expect_identical(dim(get_coords(.x, .y)), c(1L, 2L)))
 
   x <- "Fairbanks"
-  reg <- "British Columbia"
+  grp <- "British Columbia"
   err <- paste0("'", x, "' is not an available location in `locs`.")
-  expect_error(get_region(x, reg), err)
-  expect_error(get_country(x, reg), err)
-  expect_error(get_coords(x, reg), err)
+  expect_error(get_state(x, grp), err)
+  expect_error(get_country(x, grp), err)
+  expect_error(get_coords(x, grp), err)
 
   x <- c("Fairbanks")
-  reg <- "A"
-  err <- "Invalid `region`."
-  expect_error(get_region(x, reg), err)
-  expect_error(get_country(x, reg), err)
-  expect_error(get_coords(x, reg), err)
+  grp <- "A"
+  err <- "Invalid `group`."
+  expect_error(get_state(x, grp), err)
+  expect_error(get_country(x, grp), err)
+  expect_error(get_coords(x, grp), err)
 })
